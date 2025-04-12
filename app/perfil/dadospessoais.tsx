e6855a2e4575bf2b944e6591b74195e8033540c0
jpviegas <jp.viegas@hotmail.com>
2025-04-12 10:43:54 -0300

    persistir nome do localstorage

--- app/perfil/dadospessoais.tsx
+++ app/perfil/dadospessoais.tsx
@@ -2,18 +2,31 @@ import ThemedContainer from "@/components/ThemedContainer";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Text } from "@/components/ui/text";
-import { useUserStore } from "@/store/userStore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
 import { View } from "react-native";
 
 export default function DadosPessoais() {
-  const { user } = useUserStore();
+  const [name, setName] = useState("");
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const value = await AsyncStorage.getItem("name");
+        if (value !== null) {
+          setName(value);
+        }
+      } catch (e) {}
+    };
+
+    getData();
+  }, []);
 
   return (
     <ThemedContainer>
       <View className="h-full w-4/5 justify-evenly">
         <Card>
           <CardHeader>
-            <CardTitle>Nome do funcionario: {user}</CardTitle>
+            <CardTitle>Nome do funcionario: {name}</CardTitle>
           </CardHeader>
           <CardContent>
             <Text>informações pessoais</Text>

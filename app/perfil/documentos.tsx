3f89a0cde9e585830fe163f1a992d43fbb7cbf61
jpviegas <jp.viegas@hotmail.com>
2025-05-25 18:54:07 -0300

    fix: map

--- app/perfil/documentos.tsx
+++ app/perfil/documentos.tsx
@@ -1,11 +1,8 @@
-import SignaturePad from "@/components/SignaturePad";
 import React from "react";
 import { SafeAreaView } from "react-native";
 
 export default function Documentos() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <SignaturePad />
-    </SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>{/* <SignaturePad /> */}</SafeAreaView>
   );
 }

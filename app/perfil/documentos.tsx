e22eb8604f0932b9b2494e8e3e62dd72aa5a6729
jpviegas <jp.viegas@hotmail.com>
2025-05-26 09:17:01 -0300

    googlemaps api key

--- app/perfil/documentos.tsx
+++ app/perfil/documentos.tsx
@@ -1,8 +1,11 @@
+import SignaturePad from "@/components/SignaturePad";
 import React from "react";
 import { SafeAreaView } from "react-native";
 
 export default function Documentos() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>{/* <SignaturePad /> */}</SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
+      <SignaturePad />
+    </SafeAreaView>
   );
 }
